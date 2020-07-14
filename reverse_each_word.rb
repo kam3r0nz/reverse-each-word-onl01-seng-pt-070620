0a16c3a43ce5c2fb1057f21ab8691f2d2664d980
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-14 21:47:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
 def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+  new_sentence = []
+  sentence.each do |word|
+    new_sentence << word.reverse
+end
+new_sentence.join
 end
